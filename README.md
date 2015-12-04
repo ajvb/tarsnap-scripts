f0a5e21b79debf96ebfb6e3c83d5a0dd933ca654
AJ Bahnken <aj@ajvb.me>
2015-12-04 13:08:51 -0800

    Added to readme and added add_backup_to_kala.rb

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Tarsnap Scripts
 
-## MAKE SURE TO NUKE FORMER .git
+### File's
+
+1. `backup_laptop.rb` - Script for running Tarsnap on a list of directories listed in the config.json file
+2. `add_backup_to_kala.rb` - Script for adding `backup_laptop.rb` to Kala running on localhost.
+3. `config.json-example` - Example config.json file.
---
+++ add_backup_to_kala.rb
@@ -0,0 +1,33 @@
+# Based off the example ruby script in Kala's main repo.
+
+require 'net/http'
+require 'time'
+require 'json'
+
+API_URL = URI('http://127.0.0.1:8000/api/v1/job/')
+def post_to_kala(data)
+    req = Net::HTTP::Post.new(API_URL)
+    req.body = data.to_json
+    res = Net::HTTP.start(uri.host, uri.port) do |http|
+      http.request(req)
+    end
+
+    if res.body
+      JSON.parse(res.body)
+    else
+      res
+    end
+end
+
+data = {
+  name: 'tarsnap_backup',
+  command: "ruby #{Dir.pwd}/backup_laptop.rb",
+  epsilon: 'PT5S',
+  schedule: "R/#{(Time.now + 120).iso8601}/P1DT"
+}
+
+puts "Sending request to #{API_URL}"
+puts "Payload is: #{data}\n\n"
+
+job_id = post_to_kala(data)['id']
+puts "Job was created with an id of #{job_id}"
